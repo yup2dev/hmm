0c1fb20f10895e517e912094bfa5291bf04ebd8f
yup2dev <creativethony@naver.com>
2024-02-06 16:28:30 +0900

    base set start

---
+++ src/main/java/com/yup/stock_calculator/StockCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.yup.stock_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yup/stock_calculator/StockCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yup.stock_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
